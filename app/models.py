5c81588f4bd14dab4bfa13445e9a82f402602c91
timosahenru <timosahenru@gmail.com>
2023-01-02 21:15:44 +0000

    added avatar to user

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
     hackathon_participant = models.BooleanField(default=True, null=True)
-    # avatar =
+    avatar = models.ImageField(default='avatar.png')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
